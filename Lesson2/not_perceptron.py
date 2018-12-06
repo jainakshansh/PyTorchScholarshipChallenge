6f012a21653bd98b96034a5bac72419c5b949cf1
Akshansh Jain <akshanshjn9@gmail.com>
2018-12-06 20:39:29 +0530

    Coded the NOT perceptron algo.

---
+++ Lesson2/not_perceptron.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+weight1 = 0.0
+# Setting weight1 as 0 since we need to ignore the first input.
+weight2 = -1.0
+# Setting bias also as 0 since we need to focus only on 2nd input.
+bias = 0.0
+
+test_inputs = [(0, 0), (0, 1), (1, 0), (1, 1)]
+correct_outputs = [True, False, True, False]
+outputs = []
+
+for test_input, correct_output in zip(test_inputs, correct_outputs):
+    linear_combination = weight1 * test_input[0] + weight2 * test_input[1] + bias
+    output = int(linear_combination >= 0)
+    is_correct_string = 'Yes' if output == correct_output else 'No'
+    outputs.append([test_input[0], test_input[1], linear_combination, output, is_correct_string])
+
+num_wrong = len([output[4] for output in outputs if output[4] == 'No'])
+output_frame = pd.DataFrame(outputs, columns=['Input 1', '  Input 2', '  Linear Combination', '  Activation Output',
+                                              '  Is Correct'])
+if not num_wrong:
+    print('Nice!  You got it all correct.\n')
+else:
+    print('You got {} wrong.  Keep trying!\n'.format(num_wrong))
+print(output_frame.to_string(index=False))
