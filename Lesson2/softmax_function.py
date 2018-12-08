2a034a21ee1faab80ab4a0b2afcb7e3d54d82538
Akshansh Jain <akshanshjn9@gmail.com>
2018-12-08 21:16:38 +0530

    Coded the Softmax function. Tested too.

---
+++ Lesson2/softmax_function.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def softmax(L):
+    expL = np.exp(L)
+    sumExpL = sum(expL)
+    result = []
+    for i in expL:
+        result.append(i * 1.0 / sumExpL)
+    return result
+
+
+L = [5, 6, 7]
+print(softmax(L))
